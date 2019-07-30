160ba2508e02967a95b8b9ec26bb4a0c1918ffd4
srinivas reddy mula <srinivasreddymula433@gmail.com>
2019-07-30 18:00:07 +0530

    azure client ids cmd

--- var.tf
+++ var.tf
@@ -1,3 +1,9 @@
+# az login
+# az ad sp create-for-rbac --query "{ client_id: appId, client_secret: password, tenant_id: tenant }"
+# az account show --query "{ subscription_id: id }"
+
+
+
 variable "azure_subscriptionid" {
     type = "string"
     description = "your subscription id"
